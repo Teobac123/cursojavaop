4cff742bc0cbf7771b9579e0e7dd3e8e9d1715c3
DMateo <93503252+Teobac123@users.noreply.github.com>
2023-05-22 11:52:57 -0300

    Ejercicios Tema1

---
+++ Tema1/Ejercicio1.java
@@ -0,0 +1,25 @@
+package Tema1;
+
+public class Ejercicio1{
+public static void main(String[] args) {
+    
+    //Números
+    System.out.println("Datos numericos");
+    byte v1 = 8;
+    int v2 = 12;
+    float v3 = 3.66F;
+    boolean v = true;
+    System.out.println(v1);
+    System.out.println(v2);
+    System.out.println(v3);
+    System.out.println(v);
+
+    //Texto
+    System.out.println("Texto");
+    String v4 = "Hola Mundo";
+
+    System.out.println(v4);
+
+}
+
+}
